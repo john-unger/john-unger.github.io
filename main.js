f65fdaa0cbd38c8c9d0cce3d4ce2dafac484529c
John U <john.josef.unger@gmail.com>
2023-12-29 01:00:49 -0500

    use repeatable templates

--- main.js
+++ main.js
@@ -5,60 +5,58 @@ const skills = {
   "Tools": ["GitHub", "Docker", "Jira"]
 };
 
-// Mock Data for Work Experience, Education, Volunteering
-const workExperience = [
-  {
-    title: 'Software Engineer II',
-    company: 'Brightspot',
-    duration: '2 years',
-    logo: 'static/brightspot_logo.webp',
-    description: [
-      'Managed end-to-end software development lifecycle.',
-      'Contributed to all phases of the software development process.'
-    ]
-  },
-  {
-    title: 'Application Programmer',
-    company: 'Vistashare LLC',
-    duration: '3 years',
-    logo: "static/vs_logo_lg-centered.png",
-    description: [
-      'Managed end-to-end software development lifecycle.',
-      'Contributed to all phases of the software development process.'
-    ]
-  },
-];
+const experiences = {
+  "Work Experience": [
+    {
+      title: 'Software Engineer II',
+      company: 'Brightspot',
+      duration: '2 years',
+      logo: 'static/brightspot_logo.webp',
+      description: [
+        'Managed end-to-end software development lifecycle.',
+        'Contributed to all phases of the software development process.'
+      ]
+    },
+    {
+      title: 'Application Programmer',
+      company: 'Vistashare LLC',
+      duration: '3 years',
+      logo: "static/vs_logo_lg-centered.png",
+      description: [
+        'Managed end-to-end software development lifecycle.',
+        'Contributed to all phases of the software development process.'
+      ]
+    },
+  ],
+  "Education": [
+    {
+      title: 'Bachelor of Science in Computer Science',
+      company: 'University of Technology',
+      duration: '2015 - 2019',
+      logo: 'static/university_logo.png',
+      description: [
+        'Specialized in software engineering.',
+        'Graduated with honors.'
+      ]
+    },
+  ],
+  "Volunteering": [
+    {
+      title: 'Community Tech Teacher',
+      company: 'Local Library',
+      duration: '2018 - Present',
+      logo: 'static/library_logo.png',
+      description: [
+        'Conducted weekend tech workshops.',
+        'Focused on basic programming skills.'
+      ]
+    },
+  ]
+};
 
-const education = [
-  {
-    title: 'Bachelor of Science in Computer Science',
-    company: 'University of Technology',
-    duration: '2015 - 2019',
-    logo: 'static/university_logo.png',
-    description: [
-      'Specialized in software engineering.',
-      'Graduated with honors.'
-    ]
-  },
-  // ... additional education items ...
-];
-
-const volunteering = [
-  {
-    title: 'Community Tech Teacher',
-    company: 'Local Library',
-    duration: '2018 - Present',
-    logo: 'static/library_logo.png',
-    description: [
-      'Conducted weekend tech workshops.',
-      'Focused on basic programming skills.'
-    ]
-  },
-  // ... additional volunteering items ...
-];
 
 // Utility Functions
-function populateSkills(skills) {
+function populateSkills() {
   const container = document.querySelector('#skills-container');
   const template = document.querySelector('#skill-category-template').content;
 
@@ -76,33 +74,43 @@ function populateSkills(skills) {
   }
 }
 
-function populateSection(dataArray, containerId, templateId) {
-  const container = document.querySelector(`#${containerId}`);
-  const template = document.querySelector(`#${templateId}`).content;
-  const fragment = document.createDocumentFragment();
+function populateExperiences() {
+  const mainContainer = document.querySelector('.container');
+  const sectionTemplate = document.querySelector('#experience-card-template').content;
 
-  dataArray.forEach(data => {
-    const cardClone = document.importNode(template, true);
-    cardClone.querySelector('img').src = data.logo;
-    cardClone.querySelector('img').alt = `${data.company} logo`;
-    cardClone.querySelector('.card-title').textContent = data.title;
-    cardClone.querySelector('.text-base-content').textContent = `${data.company}, ${data.duration}`;
-    const ul = cardClone.querySelector('ul');
-    data.description.forEach(point => {
+  for (const sectionTitle in experiences) {
+      const sectionClone = document.importNode(sectionTemplate, true);
+      sectionClone.querySelector('h2').textContent = sectionTitle;
+      const cardsContainer = sectionClone.querySelector('div.grid');
+
+      experiences[sectionTitle].forEach(item => {
+          const card = createExperienceCard(item);
+          cardsContainer.appendChild(card);
+      });
+
+      mainContainer.appendChild(sectionClone);
+  }
+}
+
+function createExperienceCard(data) {
+  const cardTemplate = document.querySelector('#experience-card-template').content;
+  const cardClone = document.importNode(cardTemplate, true);
+
+  cardClone.querySelector('img').src = data.logo;
+  cardClone.querySelector('img').alt = `${data.company} logo`;
+  cardClone.querySelector('.card-title').textContent = data.title;
+  cardClone.querySelector('.text-base-content').textContent = `${data.company}, ${data.duration}`;
+  const ul = cardClone.querySelector('ul');
+  data.description.forEach(point => {
       const li = document.createElement('li');
       li.textContent = point;
       ul.appendChild(li);
-    });
-    fragment.appendChild(cardClone);
   });
 
-  container.appendChild(fragment);
+  return cardClone;
 }
 
-// Event Listener
 document.addEventListener("DOMContentLoaded", function () {
-  populateSkills(skills);
-  populateSection(workExperience, 'work-experience-container', 'experience-card-template');
-  populateSection(education, 'education-container', 'experience-card-template');
-  populateSection(volunteering, 'volunteering-container', 'experience-card-template');
+  populateSkills();
+  populateExperiences();
 });
