9e7e4070820c0bf17923c7a8f48d2dd220163bb3
John U <john.josef.unger@gmail.com>
2023-12-29 22:16:05 -0500

    first pass at updating my credentials

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -7,9 +7,9 @@ import ExperienceSection from './components/ExperienceSection';
 function App() {
   // Define the data for Skills and Experiences
   const skills = {
-    "Programming Languages": ["Java", "Python", "JavaScript", "JQuery", "C"],
-    "Backends & Databases": ["Node.js", "MySQL", "MongoDB"],
-    "Tools": ["GitHub", "Docker", "Jira"]
+    "Programming Languages": ["Java", "Python", "C", "JavaScript", "JQuery"],
+    "Backends & Databases": ["SQL", "Postgres", "MySQL", "Docker", "Dari", "Django", "SpringBoot",],
+    "Tools": ["GitHub", "Agile", "Jira"]
   };
 
   const experiences = {
@@ -30,16 +30,27 @@ function App() {
         duration: '3 years',
         logo: `vs_logo_lg-centered.png`,
         description: [
-          'Managed end-to-end software development lifecycle.',
-          'Contributed to all phases of the software development process.'
+          'Design and develop new functionality with RESTful API calls and object-oriented programming in Django according to priorities with the most important features',
+          'Maintained legacy code, refactored common customer uses, and optimized queries for a more seamless user experience',
+          'Pursued a self-explanatory user interface in functional design sessions',
+          'Guaranteeing common functionality with continuous improvement of an end-to-end testing framework using selenium, including reports, parallel and distributed functionality, and more'
         ]
       },
     ],
     "Education": [
       {
+        title: 'Master in Business Administration',
+        company: 'The Master\'s University',
+        duration: 'Graduated 2023',
+        logo: '',
+        description: [
+          'Gained a foundation in business practice'
+        ]
+      },
+      {
         title: 'Bachelor of Science in Computer Science',
-        company: 'University of Technology',
-        duration: '2015 - 2019',
+        company: 'James Madison University',
+        duration: 'Graduated 2017',
         logo: '',
         description: [
           'Specialized in software engineering.',
