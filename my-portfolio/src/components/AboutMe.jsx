b0dd0e551d8b9f3d8967ce6884213ca04fb46bce
John U <john.josef.unger@gmail.com>
2025-03-03 23:51:05 -0500

    update the years of experience

--- my-portfolio/src/components/AboutMe.jsx
+++ my-portfolio/src/components/AboutMe.jsx
@@ -16,7 +16,7 @@ function AboutMe() {
                 <div>
                     <h1 className="hidden md:block text-5xl font-bold mb-4">John Unger</h1>
                     <p className="text-xl leading-relaxed">
-                        I am driven by a deep passion for stewarding technology to serve and empower people, ensuring that software is not just functional but truly impactful. With nearly five years of experience in software development, I specialize in architecting scalable backend systems, optimizing performance, and enhancing user experiences. <br />
+                        I am driven by a deep passion for stewarding technology to serve and empower people, ensuring that software is not just functional but truly impactful. With over six years of experience in software development, I specialize in architecting scalable backend systems, optimizing performance, and enhancing user experiences. <br />
                         My background in Computer Science (B.S.) and Business Administration (MBA) uniquely equips me to bridge the gap between technical excellence and strategic vision. Whether refactoring legacy code, designing robust APIs, or mentoring engineers, I approach each challenge with creativity, precision, and a relentless pursuit of improvement. <br />
                         I thrive in collaborative environments, enjoy sharing knowledge, and take pride in helping teams achieve their goals through technology.
                     </p>
